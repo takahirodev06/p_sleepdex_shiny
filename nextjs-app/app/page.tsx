996b28fef09fd794538ffc5effc2abef18ef55c4
dezid <tkhrtkhrtkhr77@gmail.com>
2025-06-21 00:01:23 +0200

    first commit create app

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import {DotGothic16} from "next/font/google";
+
+const DotGothic16Font = DotGothic16({
+    weight: "400",
+    subsets: ["latin"],
+});
 
 export default function Home() {
     return (
-        <div><h1>
-            p_sleepdex_shiny
-        </h1></div>
+        <div className="main">
+            <header>
+                <div className="head_ball"></div>
+                <h1 className={DotGothic16Font.className}> ポケスリいろちがいずかん </h1>
+            </header>
+            <div className="pdex">
+                <div className="pdex_container">
+                    <div></div>
+                </div>
+            </div>
+            <footer></footer>
+        </div>
     );
 }
